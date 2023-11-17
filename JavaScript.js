80d305a0adb883589313c687d1edabdd19f1577b
Fernanda <noshynanda@gmail.com>
2023-11-17 19:20:56 -0300

    exc 2

--- JavaScript.js
+++ JavaScript.js
@@ -1,14 +1,13 @@
 function contarItensPorTipo(hotifrutis) {
-    // Inicializa contadores para cada tipo
+   
     let frutas = 0;
     let legumes = 0;
     let verduras = 0;
   
-    // Itera sobre a lista de hotifrutis
     for (let i = 0; i < hotifrutis.length; i++) {
       const hotifruti = hotifrutis[i];
   
-      // Verifica a qual tipo pertence e incrementa o contador correspondente
+      
       if (["banana", "maçã", "abacaxi", "melancia", "laranja", "limão"].includes(hotifruti)) {
         frutas++;
       } else if (["cenoura", "batata", "brócolis", "abóbora", "tomate", "alho-poró"].includes(hotifruti)) {
@@ -18,7 +17,7 @@ function contarItensPorTipo(hotifrutis) {
       }
     }
   
-    // Retorna um objeto com o número de itens de cada tipo
+    
     return {
       frutas: frutas,
       legumes: legumes,
@@ -26,7 +25,7 @@ function contarItensPorTipo(hotifrutis) {
     };
   }
   
-  // Exemplo de uso
+ 
   const hotifrutis = [
     "banana", "maçã", "abacaxi", "melancia", "laranja", "limão",
     "cenoura", "batata", "brócolis", "abóbora", "tomate", "alho-poró",
